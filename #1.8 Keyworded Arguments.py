c9c4f31b56300c73bdedb82d8d9900bd3139a655
Beginner <wook2124@naver.com>
2020-01-30 16:45:59 +0000

    #1.8 Keyworded Arguments, Complete!

--- #1.8 Keyworded Arguments.py
+++ #1.8 Keyworded Arguments.py
@@ -70,7 +70,7 @@ print(hello)
 
 # 다시 깔끔하게 수정함
 def say_hello(name, age, where_from, fav_food):
-  return f"Hello {name}, you are {age} years old? And are you from {where_from} right? So {fav_food} is your favorite food??"
+  return f"Hello {name}, you are {age} years old? And you are from {where_from} right? So {fav_food} is your favorite food??"
 
 hello = say_hello(age="27", fav_food="참치마요", where_from="Korea", name="Wook")
 print(hello)
