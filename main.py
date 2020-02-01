e10d56780bda2662378dd87687d72954d0efcc92
Beginner <wook2124@naver.com>
2020-02-01 15:21:54 +0000

    #1.12 for in, Complete!

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-def age_check(age):
-  print(f"you are {age}")
-  if age < 18:
-    print("you can't drink")
-  elif age == 18 or age == 19:
-    print("you are new to this!")
-  elif age > 20 and age < 25:
-    print("you are still kind of young")
-  else:
-    print("enjoy your drink")
+days = ("Mon", "Tue", "Wed", "Thur", "Fri")
 
-
-age_check(19)
+for letter in "JH":
+  print(letter)
