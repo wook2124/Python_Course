ff3bdd21b80190b8d2601fe209c28b9ede189002
Beginner <wook2124@naver.com>
2020-02-02 10:42:21 +0000

    #1.13 Modules, Complete!

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-days = ("Mon", "Tue", "Wed", "Thur", "Fri")
+from  calculator import plus, minus, times, division, remainder, negation, power
 
-for letter in "JH":
-  print(letter)
+print(plus(1, 2))
+print(minus(6, 3))
+print(times(2, 3))
+print(division(8, 2))
+print(remainder(10,3))
+print(negation(3, 2))
+print(power(2, 6))
