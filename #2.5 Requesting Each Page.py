2998a5546e51005f7e467961733f4b94b9b24b0a
Beginner <wook2124@naver.com>
2020-02-04 17:48:18 +0000

    #2.5 Requesting Each Page, Com!!plete!!!!!

--- #2.5 Requesting Each Page.py
+++ #2.5 Requesting Each Page.py
@@ -116,3 +116,35 @@ def extract_indeed_jobs(last_page):
   for page in range(last_page):
     result = requests.get(f"{URL}start={page * LIMIT}")
     print(result.status_code)
+
+
+
+# 마지막에 이제 일자리(jobs) 관련해서 추출할 것을
+# 미리 설정하고 마무리함. (main.py에는 indeed_jobs)
+import requests
+from bs4 import BeautifulSoup
+
+
+LIMIT = 50
+URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
+
+def extract_indeed_pages():
+  result = requests.get(URL)
+  soup = BeautifulSoup(result.text, "html.parser")
+  pagination = soup.find("div", {"class":"pagination"})
+
+  links = pagination.find_all('a')
+  pages = []
+  for link in links[:-1]:
+    pages.append(int(link.string))
+
+  max_page = pages[-1]
+  return max_page
+
+
+def extract_indeed_jobs(last_page):
+  jobs = []
+  for page in range(last_page):
+    result = requests.get(f"{URL}start={page * LIMIT}")
+    print(result.status_code)
+  return jobs
