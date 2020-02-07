5ce87ae6a360cebf323cc216df44cac21cca752c
Beginner <wook2124@naver.com>
2020-02-07 14:41:03 +0000

    #2.6 Extracting Titles, Complete!

--- #2.6 Extracting Titles.py
+++ #2.6 Extracting Titles.py
@@ -29,3 +29,37 @@ def extract_indeed_jobs(last_page):
     results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
     print(results)
   return jobs
+
+
+# for와 in을 활용해서 anchor 안에있는 title을 뽑아냄
+import requests
+from bs4 import BeautifulSoup
+
+
+LIMIT = 50
+URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
+
+def extract_indeed_pages():
+  result = requests.get(URL)
+  soup = BeautifulSoup(result.text, "html.parser")
+  pagination = soup.find("div", {"class":"pagination"})
+
+  links = pagination.find_all('a')
+  pages = []
+  for link in links[:-1]:
+    pages.append(int(link.string))
+
+  max_page = pages[-1]
+  return max_page
+
+
+def extract_indeed_jobs(last_page):
+  jobs = []
+  # for page in range(last_page):
+  result = requests.get(f"{URL}start={0 * LIMIT}")
+  soup = BeautifulSoup(result.text, "html.parser")
+  results = soup.find_all("div", {"class":"jobsearch-SerpJobCard"})
+  for result in results:
+      title = result.find("div", {"class":"title"}).find("a")["title"]
+      print(title)
+  return jobs
