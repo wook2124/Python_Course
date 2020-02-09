9ad3cfa82f60df5d349dd7417b371b1d9d42c387
Beginner <wook2124@naver.com>
2020-02-09 13:34:22 +0000

    #2.10 StackOverflow extract_jobs, Complete!

--- #2.10 StackOverflow extract_jobs.py
+++ #2.10 StackOverflow extract_jobs.py
@@ -35,3 +35,18 @@ def extract_jobs(last_page):
   for page in range(last_page):
     result = requests.get(f"{URL}&pg={page + 1}")
     print(result.status_code)
+
+
+# 두 번째 정의 수정하기
+# indeed.py와 동일하게 soup 변수를 적용시킴
+# soup 변수가 각각의 def함수에서 이름도 같지만 오류가 나지 않는 이유는
+# 딱 그 함수의 스코프 안에서만 작동하기 때문!
+# div 안에있는 class: -job을 찾고 div data-jobid 출력!
+def extract_jobs(last_page):
+  jobs = []
+  for page in range(last_page):
+    result = requests.get(f"{URL}&pg={page + 1}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class":"-job"})
+    for result in results:
+     print(result["data-jobid"])
