bacd45439219273075b0ea6e601be124e0c38e37
Beginner <wook2124@naver.com>
2020-02-09 12:39:08 +0000

    클래스 "pagination" 이름이 "s-pagination"으로 바뀌어서 오류가 났었음!

--- sof.py
+++ sof.py
@@ -7,7 +7,7 @@ URL = f"https://stackoverflow.com/jobs?q=python&sort=i"
 def get_last_page():
   result = requests.get(URL)
   soup = BeautifulSoup(result.text, "html.parser")
-  pages = soup.find("div", {"class":"pagination"}).find_all("a")
+  pages = soup.find("div", {"class":"s-pagination"}).find_all("a")
   print(pages)
 
 
