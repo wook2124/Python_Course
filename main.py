97ae4af3cf17aed01b4f847940152d37bd241756
Beginner <wook2124@naver.com>
2020-02-12 08:53:25 +0000

    #3.4 Methods part Two, Complete!

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 class Car():
-  wheels = 4
-  doors = 4
-  windows = 4
-  seats = 4
 
-  def start(self):
-    print(self.doors)
-    print("What is method.")
+  def __init__(self, **kwargs):
+    self.wheels = 4
+    self.doors = 4
+    self.windows = 4
+    self.seats = 4
+    self.color = kwargs.get("color", "black")
+    self.price = kwargs.get("price", "$20")
 
-porche = Car()
-porche.instance = "This is First instance"
-porche.start(porche)
+  def __str__(self):
+    return f"Car with {self.wheels} wheels"
+
+
+porche = Car(color = "Green", price = "$40")
+print(porche.color, porche.price)
+
+mini = Car()
+print(mini.color, mini.price)
