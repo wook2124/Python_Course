216fb5f6a14f93b8c29b36efd7e7bec17dc3947b
Beginner <wook2124@naver.com>
2020-02-22 12:48:41 +0000

    #2.10 StackOverflow extract jobs, Complete!

--- #2/#2.10 StackOverflow extract jobs.py
+++ #2/#2.10 StackOverflow extract jobs.py
@@ -1,11 +1,9 @@
 # indeed.py와 동일하게 extract_jobs을 가져오고
-# range는 int(정수)안에서만 기능하기에 
-# return int(last_page)로 str을 int로 바꿔줌
+# range는 int(정수)안에서만 기능할 수 있기때문에 return int(last_page)로 str을 int로 바꿔줌
 import requests
 from bs4 import BeautifulSoup
 
 URL = f"https://stackoverflow.com/jobs?q=python&sort=i"
-
 
 def get_last_page():
   result = requests.get(URL)
@@ -14,12 +12,10 @@ def get_last_page():
   last_page = pages[-2].get_text(strip=True)
   return int(last_page)
 
-
 def extract_jobs(last_page):
   jobs = []
   for page in range(last_page):
     print(page)
-
 
 def get_jobs():
   last_page = get_last_page()
@@ -27,9 +23,8 @@ def get_jobs():
   return jobs
 
 
-# 두 번째 정의 수정하기
-# result를 추가함 (page + 1) 해준 것은
-# 숫자가 0부터 시작하는 것을 1로 바꾸기 위함!
+# 두 번째 def 수정하기
+# result 변수명을 만들고 {page + 1} 해준 것은 숫자가 0부터 출력되는 것을 1로 바꾸기 위함!
 def extract_jobs(last_page):
   jobs = []
   for page in range(last_page):
@@ -37,10 +32,9 @@ def extract_jobs(last_page):
     print(result.status_code)
 
 
-# 두 번째 정의 수정하기
-# indeed.py와 동일하게 soup 변수를 적용시킴
-# soup 변수가 각각의 def함수에서 이름도 같지만 오류가 나지 않는 이유는
-# 딱 그 함수의 스코프 안에서만 작동하기 때문!
+# 두 번째 def 수정하기
+# indeed.py와 동일하게 soup 변수기능을 적용시킴
+# soup 변수가 각각의 def함수에서 이름도 같지만 오류가 나지 않는 이유는 딱 그 함수의 scope(범위)에서만 기능하기 때문!
 # div 안에있는 class: -job을 찾고 div data-jobid 출력!
 def extract_jobs(last_page):
   jobs = []
