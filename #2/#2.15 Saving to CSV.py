8eb91b52ad0af703bd8f6f8b7908ab7ae791610a
Beginner <wook2124@naver.com>
2020-02-26 10:13:13 +0000

    #2.15 Saving to CSV, Complete!

--- #2/#2.15 Saving to CSV.py
+++ #2/#2.15 Saving to CSV.py
@@ -1,9 +1,7 @@
 # open(파일을 열어주는 역할)함수, mode(읽기전용과 쓰기전용으로 나뉨)
-# mode에서 읽기전용은 read(r)이고, 쓰기전용은 write(w)!
-# writer = csv.writer(file)의 의미:
-# writer란 우리가 정한 'file'이라고 저장한 변수를 csv로 쓰겠다는 것을 정하는 변수
-# writer.writerow()의 의미:
-# writer 변수로 정한 쓰기권으로 write_row(행) 즉, 행을 추가한다는 뜻
+# mode에서 읽기전용은 read(r)이고, 쓰기전용은 write(w)을 뜻함
+# writer = csv.writer(file)의 의미 - writer란 우리가 정한 'file'이라고 저장한 변수를 csv로 쓰겠다는 것을 정하는 변수
+# writer.writerow()의 의미 - writer 변수로 정한 쓰기권으로 write_row(행) 즉, 행을 추가한다는 뜻
 import csv
 
 def save_file(jobs):
@@ -14,9 +12,8 @@ def save_file(jobs):
   return
 
 
-# indeed 원본!! 
-# 수업진행 할 때, 모든 페이지를 갖고오면 시간이 많이 걸려서
-# 2페이지만 가져오는 함수로 바꿔놨음.
+# indeed.py 원본
+# 수업진행 할 때, 모든 페이지를 갖고오면 시간이 많이 걸려서 2페이지만 가져오는 함수로 바꿔놨음
 import requests
 from bs4 import BeautifulSoup
 
