049c710bb46a17d6cd42393f6f987b1e4eff71af
Beginner <wook2124@naver.com>
2020-02-28 11:44:19 +0000

    #3.2 Intro to Object Oriented Programming

--- #3/#3.2 Intro to Object Oriented Programming.py
+++ #3/#3.2 Intro to Object Oriented Programming.py
@@ -6,13 +6,12 @@ class Car():
   windows = 4
   seats = 4
 
-# instance는 그 설계도로 만든 결과물을 뜻함
+# instance는 그 설계도로 만든 실제 결과물을 뜻함
 # 즉 wheels, doors, windows, seats는 Car의 기본 요소니까 설계도에 있는대로 따라가고
 # color는 차마다 다를 수 있으니까 따로 지정해준 것!
 # Car() << 이것을 instantiation이라고 함 - 설계도를 가져와서 instance를 만드는 행위
 porche = Car()
 porche.color = "Red"
-
 
 ferrari = Car()
 ferrari.color = "Yellow"
