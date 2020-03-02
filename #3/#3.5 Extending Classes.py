6a8b599fd7386eb037ce2a38c3f20ae35fd66917
Beginner <wook2124@naver.com>
2020-03-02 11:34:29 +0000

    #3.5 Extending Classes

--- #3/#3.5 Extending Classes.py
+++ #3/#3.5 Extending Classes.py
@@ -1,5 +1,5 @@
 # inherit(상속), extend(확장)하기
-# class Convertible()에서 ()에 부모 class의 이름을 적으면 됨!
+# class Convertible()에서 ()에 부모 class의 이름을 적으면 됨
 class Car():
 
   def __init__(self, **kwargs):
@@ -13,19 +13,17 @@ class Car():
   def __str__(self):
     return f"Car with {self.wheels} wheels"
 
-
 class Convertible(Car):
 
   def take_off(self):
     return "taking off"
-
 
 porche = Convertible(color = "Green", price = "$40")
 porche.take_off()
 porche.wheels
 
 
-# 결국 class Cars_son_of_son은 자기의 할아버지 격인 Car의 모든 method를 갖게됨!
+# class Cars_son_of_son은 자기의 할아버지 격인 Car의 모든 method를 갖게됨
 class Car():
 
   def __init__(self, **kwargs):
@@ -39,25 +37,22 @@ class Car():
   def __str__(self):
     return f"Car with {self.wheels} wheels"
 
-
 class Convertible(Car):
 
   def take_off(self):
     return "taking off"
-
 
 class Cars_son_of_son(Convertible):
   pass
 
-
 porche = Convertible(color = "Green", price = "$40")
 porche.take_off()
 porche.wheels
 
 
-# override한 것 다시 override하기
-# 그러나 이렇게 override(재정의)하게 되면 말그대로 color같이 원래 갖고있는 속성도 사라지게 됨!
-# 그저 __init__에 항목을 하나 더 추가하고 싶었을 뿐인데도!
+# override(재정의)한 것 다시 override하기
+# 그러나 이렇게 override하게 되면 말그대로 color같이 원래 갖고있는 속성도 사라지게 됨
+# 그저 __init__에 항목을 하나 더 추가하고 싶었을 뿐인데도
 class Car():
 
   def __init__(self, **kwargs):
@@ -70,7 +65,6 @@ class Car():
 
   def __str__(self):
     return f"Car with {self.wheels} wheels"
-
 
 class Convertible(Car):
 
@@ -83,17 +77,15 @@ class Convertible(Car):
   def __str__(self):
     return f"Car with no roof"
 
-
 class Cars_son_of_son(Convertible):
   pass
-
 
 porche = Convertible(color = "Green", price = "$40")
 print(porche.color)
 
 
-# super().를 통해서 부모클래스에 접근한 뒤 __init__ method를 호출함
-# super는 father, upper class에 해당한다! (super is father)
+# super()를 통해서 부모클래스에 접근한 뒤 .__init__ method를 호출함
+# super = father, upper class
 class Car():
 
   def __init__(self, **kwargs):
@@ -106,7 +98,6 @@ class Car():
 
   def __str__(self):
     return f"Car with {self.wheels} wheels"
-
 
 class Convertible(Car):
 
@@ -120,10 +111,8 @@ class Convertible(Car):
   def __str__(self):
     return f"Car with no roof"
 
-
 class Cars_son_of_son(Convertible):
   pass
-
 
 porche = Convertible(color = "Green", price = "$40")
 print(porche.color)
